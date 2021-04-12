32c2be84be77207529d5dcbd5fc9d07bbf62c088
eunseo2 <20180972@sungshin.ac.kr>
2021-04-12 16:37:47 +0900

    googleStrategy

---
+++ src/auth/passport/google.strategy.ts
@@ -0,0 +1,37 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, VerifyCallback } from 'passport-google-oauth2';
+import { config } from 'dotenv';
+
+import { Injectable } from '@nestjs/common';
+
+config();
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor() {
+    super({
+      clientID: '',
+      clientSecret: '',
+      callbackURL: 'http://localhost:3000/auth/redirect',
+      scope: ['email', 'profile'],
+    });
+  }
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    done: VerifyCallback,
+  ): Promise<any> {
+    const { name, emails, id, picture, displayName } = profile;
+    //console.log(profile);
+    const user = {
+      naame: displayName,
+      GoogleID: id,
+      profile: picture,
+      email: emails[0].value,
+      //username: name,
+      accessToken,
+    };
+    done(null, user);
+  }
+}
