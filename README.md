bd15e12af963ebc95aaaf923d773ea64bea20cfa
eunseo2 <20180972@sungshin.ac.kr>
2021-04-12 15:56:18 +0900

    mariadb connect

--- README.md
+++ README.md
@@ -3,8 +3,16 @@
 ```bash
 $ npm install
 $ npm i @nestjs/config
+
+google login
 $ npm i --save @nestjs/passport passport passport-google-oauth2
 $ npm install -D @types/passport-google-oauth20
+
+typeorm
+$ npm install --save @nestjs/typeorm typeorm pg
+
+mariadb
+npm install mysql --save
 ```
 
 ## Running the app
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,23 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env.development',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PW,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true,
     }),
     AuthModule,
   ],
